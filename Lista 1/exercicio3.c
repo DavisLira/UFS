35fc58b613ba207b5815c31787e6d9a8dc16522f
Davi Lira <112901144+DavisLira@users.noreply.github.com>
2023-11-30 11:45:27 +0000

    Criação do exercicio 3

---
+++ Lista 1/exercicio3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    // Declaração das variáveis
+    int quantidade = 3; // Quantidade das doses que faltam a ser tomadas
+    int ano, periodicidade;
+
+    // Solicita o ano da 1° dose
+    printf("Qual foi o ano da 1° dose? ");
+    scanf("%d", &ano);
+
+    // Solicita o intervalo entre as doses
+    printf("Qual o intervalo entre as doses? ");
+    scanf("%d", &periodicidade);
+
+    // Mostra os proximos anos que irá tomar a vacina
+    for (quantidade; quantidade > 0; quantidade--)
+    {
+        ano = ano + periodicidade;
+        printf("%d\n", ano);
+    }
+
+    return 0;
+}
