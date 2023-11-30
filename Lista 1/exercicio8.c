8dc0785a5bc1602ee0bcd05085393aefa931a07f
Davi Lira <112901144+DavisLira@users.noreply.github.com>
2023-11-30 12:04:24 +0000

    Criação do exercicio 8

---
+++ Lista 1/exercicio8.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+  // Declara as variáveis
+  float comprimento, largura, area;
+  
+  // Solicita o tamanho do comprimento
+  printf ("Qual é o comprimento do retângulo em m? ");
+  scanf("%f", &comprimento);
+  
+  // Solicita a largura 
+  printf ("Qual é a largura do retângulo m? ");
+  scanf("%f", &largura);
+  
+  // Calcula a área do retângulo
+  area = comprimento * largura;
+  
+  // Mostra a área
+  printf("Esse retângulo tem %.2f m² de área\n", area);
+  
+  return 0;
+}
