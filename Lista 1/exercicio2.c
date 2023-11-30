5e610fda87ecce63bc66c8176a603ff46aeaefe9
Davi Lira <112901144+DavisLira@users.noreply.github.com>
2023-11-30 11:37:40 +0000

    Criação do exercicio 2

---
+++ Lista 1/exercicio2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    // Declaração das variáveis
+    float pesoAtual, pesoIdeal, perdaMensal, meses, diferencaPeso;
+
+    //Solicita ao usuário o peso atual em kg
+    printf("Digite o peso atual em kg: ");
+    scanf("%f", &pesoAtual);
+
+    // Solicita ao usuário o peso ideal em kg
+    printf("Digite o peso ideal em kg: ");
+    scanf("%f", &pesoIdeal);
+
+    // Solicita ao usuário a perda mensal
+    printf("Digite a perda mensal em kg: ");
+    scanf("%f", &perdaMensal);
+
+    // Calcula a diferença dos pesos
+    diferencaPeso = pesoAtual - pesoIdeal;
+
+    // Calcula quantos meses demora para perder o peso
+    meses = diferencaPeso / perdaMensal;
+
+    // Mostra ao usuário em quantos meses irá perder o peso
+    printf("Você vai demorar %.1f meses para perder %.2fkg\n\n", meses, diferencaPeso);
+}
