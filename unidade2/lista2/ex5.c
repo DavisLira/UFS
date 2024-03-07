874747d5211f5273c1ebec74f22253aac798b09a
Davi Lira <112901144+DavisLira@users.noreply.github.com>
2024-03-07 22:25:57 +0000

    correção atividade 5

--- unidade2/lista2/ex5.c
+++ unidade2/lista2/ex5.c
@@ -5,12 +5,16 @@
 int verificaCod(char matricula[]) {
 
     char cod[3] = "99";
+    int codCerto = 0;
 
     for (int i = 4; i < 6; i++) {
         int j = 0;
         if (matricula[i] == cod[j++])
         {
-            return 1;
+            codCerto++;
+            if(codCerto == 2){
+                return 1;
+            }
         }
     }
     
@@ -36,12 +40,12 @@ void verifica(char matricula[], char ano[], int *quantidade) {
 int main()
 {
     char matricula[11], ano[5];
-    int quantidade;
+    int quantidade = 0;
 
     printf("Digite o ano de busca dos alunos de gerontologia (COD: 99): ");
     scanf(" %4[^\n]s", ano);
     
-    for (int i = 1; i <= 100; i++) {
+    for (int i = 1; i <= 3; i++) {
         printf("Digite a matricula: ");
         scanf(" %10[^\n]", matricula);
 
