d9c4e09743fe03bcb2087eae9230d39e05f7f267
Davi Lira <112901144+DavisLira@users.noreply.github.com>
2024-05-11 04:46:06 +0000

    ex1

---
+++ ed/unidade 1/ex1.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main () {
+    int *numeros, *mult6, quantos, qtddiv = 0, total = 0;
+
+    printf("Digite a quantidade de itens: ");
+    scanf("%d", &quantos);
+
+    numeros = (int*) malloc(quantos*sizeof(int));
+    mult6 = (int*) malloc(qtddiv*sizeof(int));
+
+    if(!numeros) {
+        printf("Memória nao alocada. \nEncerrando programa...\n");
+        return;
+    }
+
+    for (int i = 0; i < quantos; i++)
+    {
+        printf("Digite o %d° valor: ", i+1);
+        scanf("%d", &numeros[i]);
+        total += numeros[i];
+        if (numeros[i] % 6 == 0) 
+        {
+            mult6 = (int*) realloc(mult6, (qtddiv + 1) * sizeof(int));
+            mult6[qtddiv] = numeros[i];
+            qtddiv++;
+        }
+    }
+    
+    printf("\n*****************************\n");
+    printf("  Média dos números lidos: %.2f\n", (float)total/quantos);
+    printf("  Números multiplos de 6:");
+    
+    for (int i = 0; i < qtddiv; i++)
+    {
+        printf(" %d", mult6[i]);
+    }
+    if (qtddiv == 0) printf(" 0");
+
+    printf("\n\n");
+
+    free(numeros);
+    free(mult6);
+}
