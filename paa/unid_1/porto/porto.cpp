6adadeeda916df483d5aa5ee2830c9b2d4ff8259
davilirasantana86 <davilirasantana86@gmail.com>
2025-01-17 12:12:31 +0000

    finalização do projeto

--- paa/unid_1/porto/porto.cpp
+++ paa/unid_1/porto/porto.cpp
@@ -7,14 +7,14 @@ using namespace std;
 
 struct Container {
     int ordem;
-    string numero;
+    string codigo;
     string cnpjCadastrado;
     string cnpjSelecionado;
     int pesoCadastrado = 0;
     int pesoSelecionado = 0;
     bool divergenciaCnpj = false;
     bool divergenciaPeso = false;
-
+    int percentualPeso = 0;
 };
 
 void lerArquivo(ifstream& arquivo, Container*& cadastrados, int& numC, Container*& selecionados, int& numS) {
@@ -22,15 +22,75 @@ void lerArquivo(ifstream& arquivo, Container*& cadastrados, int& numC, Container
     cadastrados = new Container[numC];
     for (int i = 0; i < numC; ++i) {
         cadastrados[i].ordem = i;
-        arquivo >> cadastrados[i].numero >> cadastrados[i].cnpjCadastrado >> cadastrados[i].pesoCadastrado;
+        arquivo >> cadastrados[i].codigo >> cadastrados[i].cnpjCadastrado >> cadastrados[i].pesoCadastrado;
     }
 
     arquivo >> numS;
     selecionados = new Container[numS];
     for (int i = 0; i < numS; ++i) {
-        selecionados[i].ordem = i;
-        arquivo >> selecionados[i].numero >> selecionados[i].cnpjSelecionado >> selecionados[i].pesoSelecionado;
+        arquivo >> selecionados[i].codigo >> selecionados[i].cnpjSelecionado >> selecionados[i].pesoSelecionado;
     }
+}
+
+void mergeAlfabetica(Container* entrada, int inicio, int meio, int fim) {
+    int n1 = meio - inicio + 1;
+    int n2 = fim - meio;
+
+    Container* esquerda = new Container[n1];
+    Container* direita = new Container[n2];
+
+    for (int i = 0; i < n1; ++i) esquerda[i] = entrada[inicio + i];
+    for (int i = 0; i < n2; ++i) direita[i] = entrada[meio + 1 + i];
+
+    int i = 0, j = 0, k = inicio;
+
+    while (i < n1 && j < n2) {
+        if (esquerda[i].codigo <= direita[j].codigo) {
+            entrada[k++] = esquerda[i++];
+        } else {
+            entrada[k++] = direita[j++];
+        }
+    }
+
+    while (i < n1) {
+        entrada[k++] = esquerda[i++];
+    }
+
+    while (j < n2) {
+        entrada[k++] = direita[j++];
+    }
+
+    delete[] esquerda;
+    delete[] direita;
+}
+
+void mergeSortAlfabetica(Container* entrada, int inicio, int fim) {
+    if (inicio < fim) {
+        int meio = inicio + (fim - inicio) / 2;
+        mergeSortAlfabetica(entrada, inicio, meio);
+        mergeSortAlfabetica(entrada, meio + 1, fim);
+        mergeAlfabetica(entrada, inicio, meio, fim);
+    }
+}
+
+int buscaBinaria(Container* lista, int esq, int dir, const string& codigo) {
+    while (esq <= dir) {
+        int mid = esq + (dir - esq) / 2;
+
+        if (lista[mid].codigo == codigo) {
+            return mid;
+        }
+
+        if (lista[mid].codigo > codigo) {
+            dir = mid - 1;
+        }
+
+        else {
+            esq = mid + 1;
+        }
+    }
+    
+    return -1;
 }
 
 void filtrarIrregulares(Container* cadastrados, int numC, Container* selecionados, int numS, Container*& irregulares, int& numI) {
@@ -38,36 +98,26 @@ void filtrarIrregulares(Container* cadastrados, int numC, Container* selecionado
     irregulares = new Container[numS];
 
     // Loop para verificar os cadastrados e selecionados
-    for (int i = 0; i < numC; ++i) {
-        // Flag para garantir que um container seja adicionado apenas uma vez
-        bool adicionado = false;
+    for (int c = 0; c < numC; ++c) {
+        int s = buscaBinaria(selecionados, 0, numS - 1, cadastrados[c].codigo);
 
-        Container atual = selecionados[j];
-        if (cadastrados[i].numero == selecionados[j].numero) {
+        if (s == -1) {
+            continue;
+        } else if (cadastrados[c].codigo == selecionados[s].codigo) {
+            int diferencaPeso = abs(cadastrados[c].pesoCadastrado - selecionados[s].pesoSelecionado);
 
             // Verificar divergência de CNPJ
-            if (cadastrados[i].cnpjCadastrado != selecionados[j].cnpjSelecionado) {
-                selecionados[j].cnpjCadastrado = cadastrados[i].cnpjCadastrado;
-                selecionados[j].divergenciaCnpj = true;
-                // Adiciona à lista de irregulares apenas se ainda não foi adicionado
-                if (!adicionado) {
-                    irregulares[numI++] = selecionados[j]; // Adiciona à lista de irregulares
-                    adicionado = true;  // Marca como adicionado
-                }
+            if (cadastrados[c].cnpjCadastrado != selecionados[s].cnpjSelecionado) {
+                selecionados[s].cnpjCadastrado = cadastrados[c].cnpjCadastrado;
+                selecionados[s].divergenciaCnpj = true;
+                irregulares[numI++] = selecionados[s];
+            } else if (diferencaPeso > cadastrados[c].pesoCadastrado * 0.1) {
+                int diferencaPercentual = round(diferencaPeso * 100.0 / cadastrados[c].pesoCadastrado);
+                selecionados[s].percentualPeso = diferencaPercentual;
+                selecionados[s].pesoCadastrado = cadastrados[c].pesoCadastrado;
+                selecionados[s].divergenciaPeso = true;
+                irregulares[numI++] = selecionados[s];
             }
-
-            // Verificar divergência de peso
-            if (abs(cadastrados[i].pesoCadastrado - selecionados[j].pesoSelecionado) > cadastrados[i].pesoCadastrado * 0.1) {
-                selecionados[j].pesoCadastrado = cadastrados[i].pesoCadastrado;
-                selecionados[j].divergenciaPeso = true;
-                // Adiciona à lista de irregulares apenas se ainda não foi adicionado
-                if (!adicionado) {
-                    irregulares[numI++] = selecionados[j]; // Adiciona à lista de irregulares
-                    adicionado = true;  // Marca como adicionado
-                }
-            }
-
-            break; // Já achou o item correspondente, pode sair do loop
         }
     }
 }
@@ -96,8 +146,15 @@ void merge(Container* entrada, int inicio, int meio, int fim) {
         } else if (direita[j].divergenciaCnpj) {
             entrada[k++] = direita[j++];
         } else {
-            if (esquerda[i].divergenciaPeso) {
+            if (esquerda[i].divergenciaPeso && direita[j].divergenciaPeso) {
+                if (esquerda[i].percentualPeso >= direita[j].percentualPeso) {
+                    entrada[k++] = esquerda[i++];
+                } else {
+                    entrada[k++] = direita[j++];
+                }
+            } else if (esquerda[i].divergenciaPeso) {
                 entrada[k++] = esquerda[i++];
+
             } else {
                 entrada[k++] = direita[j++];
             }
@@ -129,35 +186,58 @@ void mergeSort(Container* entrada, int inicio, int fim) {
     }
 }
 
-int main() {
-    ifstream arquivo("porto.input");
-    if (!arquivo) {
-        cerr << "Erro ao abrir o arquivo!" << endl;
+int main(int argc, char* argv[]) {
+    // Verificar se os argumentos foram fornecidos
+    if (argc != 3) {
+        cerr << "Uso: " << argv[0] << " <arquivo_entrada> <arquivo_saida>" << endl;
         return 1;
     }
+    // Abrir o arquivo de entrada
+    ifstream arquivoEntrada(argv[1]);
+    if (!arquivoEntrada) {
+        cerr << "Erro ao abrir o arquivo de entrada: " << argv[1] << endl;
+        return 1;
+    }
+    // Abrir o arquivo de saída
+    ofstream arquivoSaida(argv[2]);
+    if (!arquivoSaida) {
+        cerr << "Erro ao criar o arquivo de saída: " << argv[2] << endl;
+        return 1;
+    }
+
+    cout << "Inicio" << endl;
 
     Container* cadastrados = nullptr;
     Container* selecionados = nullptr;
     Container* irregulares = nullptr;
     int numC = 0, numS = 0, numI = 0;
 
-    // Ler dados do arquivo
-    lerArquivo(arquivo, cadastrados, numC, selecionados, numS);
-    arquivo.close();
+    // Ler dados do arquivo de entrada
+    lerArquivo(arquivoEntrada, cadastrados, numC, selecionados, numS);
+    arquivoEntrada.close();
+
+    Container* selecionadosAlf = new Container[numS];
+    for (int i = 0; i < numS; ++i) {
+        selecionadosAlf[i] = selecionados[i];
+    }
+
+    // Ordenar a lista de cadastrados
+    mergeSortAlfabetica(selecionadosAlf, 0, numS - 1);
 
     // Ordenar a lista de selecionados e identificar divergências
-    filtrarIrregulares(cadastrados, numC, selecionados, numS, irregulares, numI);
+    filtrarIrregulares(cadastrados, numC, selecionadosAlf, numS, irregulares, numI);
 
+    // Ordenar a lista de irregulares
     mergeSort(irregulares, 0, numI - 1);
 
-    // Exibir os resultados de divergências
+    // Escrever os resultados de divergências no arquivo de saída
     for (int i = 0; i < numI; ++i) {
         if (irregulares[i].divergenciaCnpj) {
-            cout << irregulares[i].numero << ":" << irregulares[i].cnpjCadastrado << "<->" << irregulares[i].cnpjSelecionado << endl;
+            arquivoSaida << irregulares[i].codigo << ":" << irregulares[i].cnpjCadastrado << "<->" << irregulares[i].cnpjSelecionado << endl;
         }
         if (irregulares[i].divergenciaPeso) {
-            cout << irregulares[i].numero << ":" << abs(irregulares[i].pesoSelecionado - irregulares[i].pesoCadastrado) << "kg (" 
-                 << round(abs(irregulares[i].pesoSelecionado - irregulares[i].pesoCadastrado) * 100.0 / irregulares[i].pesoCadastrado) << "%)" << endl;
+            arquivoSaida << irregulares[i].codigo << ":" << abs(irregulares[i].pesoSelecionado - irregulares[i].pesoCadastrado) << "kg ("
+                         << irregulares[i].percentualPeso << "%)" << endl;
         }
     }
 
@@ -165,6 +245,11 @@ int main() {
     delete[] cadastrados;
     delete[] selecionados;
     delete[] irregulares;
+
+    // Fechar o arquivo de saída
+    arquivoSaida.close();
+
+    cout << "Fim" << endl;
 
     return 0;
 }
