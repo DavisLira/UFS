d7c595b8f4b456dbbea1b85ffd7f40b4cdfa5d73
Davi Lira <davilirasantana86@gmail.com>
2025-02-16 18:36:56 +0000

    finalização

--- paa/unid_2/mochila/main.cpp
+++ paa/unid_2/mochila/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <cmath>
 #include <iomanip>  // Para controle de casas decimais
 #include <fstream>  // Para ler e escrever arquivos
 
@@ -9,15 +10,15 @@ using namespace std;
 struct Pacote {
     string codigo;  // Identificador do pacote
     float valor;    // Valor do pacote
-    int peso;       // Peso do pacote
-    int volume;     // Volume do pacote
+    float peso;       // Peso do pacote
+    float volume;     // Volume do pacote
     bool alocado;   // Flag para indicar se o pacote foi alocado a algum carro
 };
 
 struct Carro {
     string placa;    // Placa do carro
-    int peso_max;    // Peso máximo que o carro pode carregar
-    int volume_max;  // Volume máximo que o carro pode carregar
+    float peso_max;    // Peso máximo que o carro pode carregar
+    float volume_max;  // Volume máximo que o carro pode carregar
     Pacote* pacotes; // Lista de pacotes que o carro pode carregar
     int num_pacotes; // Número de pacotes para este carro
 };
@@ -63,74 +64,79 @@ void ler_arquivo(ifstream& input, int& num_carros, int& num_pacotes, Carro*& car
     }
 }
 
-void knapsack(Carro& carro, Pacote* pacotes, int num_pacotes, Pacote*& pacotes_r, int& qtd_r) {
-    int W = carro.peso_max;
-    int V = carro.volume_max;
-    int totalCelulas = (W + 1) * (V + 1);
-    
-    // Aloca as matrizes DP e de escolhas como arrays 1D.
-    float* dp = new float[totalCelulas];
-    int* choice = new int[totalCelulas];
-    
-    // Inicializa as matrizes: dp com 0 e choice com -1.
-    for (int i = 0; i < totalCelulas; i++) {
-        dp[i] = 0.0f;
-        choice[i] = -1;
+void knapsack(Carro& carro, Pacote* pacotes, int num_pacotes) {
+    // Alocação dinâmica da tabela DP para evitar estouro de pilha
+    float*** dp = new float**[num_pacotes + 1];
+    for (int i = 0; i <= num_pacotes; i++) {
+        dp[i] = new float*[(int)carro.peso_max + 1];
+        for (int w = 0; w <= (int)carro.peso_max; w++) {
+            dp[i][w] = new float[(int)carro.volume_max + 1](); // Inicializa com 0
+        }
     }
     
-    // Percorre cada pacote não alocado
-    for (int i = 0; i < num_pacotes; i++) {
-        if (pacotes[i].alocado) {
-            continue;  // Pula pacotes já alocados para outro carro
-        }
-        int wt = pacotes[i].peso;
-        int vol = pacotes[i].volume;
-        float val = pacotes[i].valor;
-        
-        // Para cada pacote, percorre as capacidades de forma decrescente para evitar múltiplas contagens
-        for (int w = W; w >= wt; w--) {
-            for (int v = V; v >= vol; v--) {
-                int idxAtual = w * (V + 1) + v;
-                int idxPrev = (w - wt) * (V + 1) + (v - vol);
-                float novoValor = dp[idxPrev] + val;
-                if (novoValor > dp[idxAtual]) {
-                    dp[idxAtual] = novoValor;
-                    choice[idxAtual] = i;  // Registra o índice do pacote que melhorou o valor
+
+    // Construindo a tabela DP
+    for (int i = 1; i <= num_pacotes; i++) {
+        if (pacotes[i - 1].alocado) continue; // Ignora pacotes já alocados
+
+        for (int w = 0; w <= (int)carro.peso_max; w++) {
+            for (int v = 0; v <= (int)carro.volume_max; v++) {
+                if (pacotes[i - 1].peso <= w && pacotes[i - 1].volume <= v) {
+                    int new_w = w - (int)pacotes[i - 1].peso;
+                    int new_v = v - (int)pacotes[i - 1].volume;
+                    
+                    dp[i][w][v] = max(
+                        pacotes[i - 1].valor + (new_w >= 0 && new_v >= 0 ? dp[i - 1][new_w][new_v] : 0),
+                        dp[i - 1][w][v]
+                    );
+                } else {
+                    dp[i][w][v] = dp[i - 1][w][v];
                 }
             }
         }
     }
-    
-    // Retropropagação (backtracking) para recuperar os pacotes selecionados
-    qtd_r = 0;
-    int w = W, v = V;
-    while (w >= 0 && v >= 0) {
-        int idxAtual = w * (V + 1) + v;
-        if (choice[idxAtual] == -1) { // Se não houve escolha para este estado, encerra
-            break;
+
+    // Recuperando os pacotes selecionados
+    int w = (int)carro.peso_max, v = (int)carro.volume_max;
+    int num_pac_esc = 0;
+    Pacote* pacotes_escolhidos = new Pacote[num_pacotes];
+
+    for (int i = num_pacotes; i > 0 && w > 0 && v > 0; i--) {
+        if (pacotes[i - 1].alocado) continue; // Pula pacotes já alocados
+
+        if (dp[i][w][v] != dp[i - 1][w][v]) {
+            pacotes_escolhidos[num_pac_esc++] = pacotes[i - 1];
+            pacotes[i - 1].alocado = true; // Marca o pacote como alocado
+
+            w -= (int)pacotes[i - 1].peso;
+            v -= (int)pacotes[i - 1].volume;
         }
-        int idxPacote = choice[idxAtual];
-        pacotes_r[qtd_r++] = pacotes[idxPacote];  // Adiciona o pacote à lista do carro
-        pacotes[idxPacote].alocado = true;           // Marca o pacote como alocado
-        // Atualiza a capacidade remanescente
-        w -= pacotes[idxPacote].peso;
-        v -= pacotes[idxPacote].volume;
     }
-    
+
+    // Atribuir pacotes selecionados ao carro
+    carro.pacotes = pacotes_escolhidos;
+    carro.num_pacotes = num_pac_esc;
+
+    // Liberar memória alocada para dp
+    for (int i = 0; i <= num_pacotes; i++) {
+        for (int w = 0; w <= (int)carro.peso_max; w++) {
+            delete[] dp[i][w];
+        }
+        delete[] dp[i];
+    }
     delete[] dp;
-    delete[] choice;
 }
 
 void verificar_disponibilidade(Carro* carros, int num_carros, Pacote* pacotes, int num_pacotes) {
     for (int i = 0; i < num_carros; i++) {
-        knapsack(carros[i], pacotes, num_pacotes, carros[i].pacotes, carros[i].num_pacotes);
+        knapsack(carros[i], pacotes, num_pacotes);
     }
 }
 
 void pegar_dados_pacotes(
         Pacote* pacotes, int num,
         Pacote* pacotes_r, int& qtd_r,
-        float& valor_r, int& peso_r, int& volume_r
+        float& valor_r, float& peso_r, float& volume_r
     ) {
 
     for (int i = 0; i < num; i++) {
@@ -144,8 +150,8 @@ void pegar_dados_pacotes(
 }
 
 void mostrar_pacotes(Pacote* pct, int qtd, ofstream& output) {
-    for (int i = 0; i < qtd; i++) {
-        if (i > 0) {
+    for (int i = qtd - 1; i >= 0; i--) { // Percorre de trás para frente
+        if (i < qtd - 1) { // Adiciona a vírgula entre os elementos
             output << ",";
         }
         output << pct[i].codigo;
@@ -156,30 +162,37 @@ void mostrar_carros(ofstream& output, Carro* carros, int num_carros) {
     for (int i = 0; i < num_carros; i++) {
         Pacote* pacotes_c = new Pacote[carros[i].num_pacotes];
         float valor_c = 0;
-        int peso_c = 0;
-        int volume_c = 0;
+        float peso_c = 0;
+        float volume_c = 0;
         int qtd_c = 0;
 
         pegar_dados_pacotes(carros[i].pacotes, carros[i].num_pacotes, pacotes_c, qtd_c, valor_c, peso_c, volume_c);
 
-        output << "[" << carros[i].placa << "]R$" << valor_c << "," << peso_c << "KG," << volume_c << "L->";
+        int peso_porct = round(peso_c / carros[i].peso_max * 100);
+        int vol_porct = round(volume_c / carros[i].volume_max * 100);
+
+        output << "[" << carros[i].placa << "]R$" 
+        << std::fixed << std::setprecision(2) << valor_c << ","
+        << std::fixed << std::setprecision(0) << peso_c << "KG("
+        << peso_porct << "%)," << volume_c << "L(" << vol_porct << "%)->";
 
         mostrar_pacotes(pacotes_c, qtd_c, output);
         output << endl;
     }
-
 }
 
 void mostrar_pacotes_pendentes(ofstream& output, Pacote* pacotes, int num_pacotes) {
     Pacote* pacotes_p = new Pacote[num_pacotes];
     float valor_p = 0;
-    int peso_p = 0;
-    int volume_p = 0;
+    float peso_p = 0;
+    float volume_p = 0;
     int qtd_p = 0;
 
     pegar_dados_pacotes(pacotes, num_pacotes, pacotes_p, qtd_p, valor_p, peso_p, volume_p);
 
-    output << "PENDENTE:R$" << valor_p << "," << peso_p << "KG," << volume_p << "L->";
+    output << "PENDENTE:R$" << 
+        std::fixed << std::setprecision(2) << valor_p << "," << 
+        std::fixed << std::setprecision(0) << peso_p << "KG," << volume_p << "L->";
 
     mostrar_pacotes(pacotes_p, qtd_p, output);
 }
