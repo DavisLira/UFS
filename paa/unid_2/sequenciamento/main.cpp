52901a04e2451f212809d8d743b00d757aa1e580
davilirasantana86 <davilirasantana86@gmail.com>
2025-02-19 23:50:57 +0000

    inicio da busca

--- paa/unid_2/sequenciamento/main.cpp
+++ paa/unid_2/sequenciamento/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <ostream>
 #include <fstream>
 #include <string>
 #include <chrono>
@@ -15,7 +14,7 @@ struct Doenca {
     string nome;
     int num_genes;
     Gene* genes;
-    int prob;    // Porcentagem inteira de genes detectados
+    int prob;
 };
 
 bool abrir_arquivos(const char* entrada, const char* saida, ifstream &input, ofstream &output) {
@@ -50,16 +49,65 @@ void ler_arquivo(ifstream &input, string &dna, int &tam_subcadeia, Doenca *&doen
     input.close();
 }
 
-int contar_genes_detectados(string dna, Gene gene, int tam_subcadeia) {
-    int num_genes_detectados = 0;
+void mostrar_encontros(int* lista_encontros, int count) {
+    for (int i = 0; i < count; i++) {
+        cout << lista_encontros[i] << " ";
+    }
+}
+
+void calcular_tabela(int* tabela_transicao, const string& gene) {
+    int tamanho_gene = gene.size();
+    tabela_transicao[0] = -1;
+    
+    for (int i = 1, j = -1; i < tamanho_gene; i++) {
+        while (j >= 0 && gene[j + 1] != gene[i]) 
+            j = tabela_transicao[j];
+        if (gene[j + 1] == gene[i]) 
+            j++;
+        tabela_transicao[i] = j;
+    }
+}
+
+int KMP(int* tabela_transicao, int* lista_encontros, string dna, const string& gene) {
+    int tamanho_dna = dna.size(), tamanho_gene = gene.size(), count = 0;
+    calcular_tabela(tabela_transicao, gene);
+    for (int i = 0, j = -1; i < tamanho_dna; i++) {
+        while (j >= 0 && gene[j + 1] != dna[i]) 
+            j = tabela_transicao[j];
+        if (gene[j + 1] == dna[i]) 
+            j++;
+        if (j == tamanho_gene - 1) { 
+            lista_encontros[count++] = i - tamanho_gene + 1;
+            j = tabela_transicao[j];
+        }
+    }
+    mostrar_encontros(lista_encontros, count);
+    cout << " - "<< count << endl;
+    return count;
+}
+
+int contar_genes_detectados(string dna, string gene, int tam_subcadeia) {
+    int tamanho_gene = gene.size();
+    int* tabela_transicao = new int[tamanho_gene]; 
+    int* lista_encontros = new int[dna.size()];
+
+    int num_genes_detectados = KMP(tabela_transicao, lista_encontros, dna, gene);
+    
+    delete[] tabela_transicao;
+    delete[] lista_encontros;
+    
+    return num_genes_detectados;
 }
 
 void calcular_probabilidade(string dna, Doenca *doencas, int num_doencas, int tam_subcadeia) {
-    int num_genes_detectados = 0;
     for (int i = 0; i < num_doencas; i++) {
+        cout << "Calculando i = " << i << ": " << doencas[i].nome << endl;
+        int num_genes_detectados = 0;
         for (int j = 0; j < doencas[i].num_genes; j++) {
-            num_genes_detectados += contar_genes_detectados(dna, doencas[i].genes[j], tam_subcadeia);
+            cout << "Lendo gene j = " << j << ": " << doencas[i].genes[j].sequencia << endl;
+            num_genes_detectados += contar_genes_detectados(dna, doencas[i].genes[j].sequencia, tam_subcadeia);
         }
+        doencas[i].prob = (num_genes_detectados * 100) / doencas[i].num_genes;
     }
 }
 
@@ -69,62 +117,36 @@ void mostrar_doencas(ofstream &output, Doenca *doencas, int num_doencas) {
     }
 }
 
-// Função para mesclar duas sublistas
 void merge(Doenca *doencas, int left, int mid, int right) {
     int n1 = mid - left + 1;
     int n2 = right - mid;
-
     Doenca *leftArray = new Doenca[n1];
     Doenca *rightArray = new Doenca[n2];
 
-    // Copia os dados para as sublistas temporárias
-    for (int i = 0; i < n1; i++) {
-        leftArray[i] = doencas[left + i];
-    }
-    for (int j = 0; j < n2; j++) {
-        rightArray[j] = doencas[mid + 1 + j];
-    }
+    for (int i = 0; i < n1; i++) leftArray[i] = doencas[left + i];
+    for (int j = 0; j < n2; j++) rightArray[j] = doencas[mid + 1 + j];
 
-    // Mescla as sublistas de volta para o vetor original
     int i = 0, j = 0, k = left;
     while (i < n1 && j < n2) {
-        if (leftArray[i].prob >= rightArray[j].prob) { // Ordenação decrescente por probabilidade
-            doencas[k] = leftArray[i];
-            i++;
-        } else {
-            doencas[k] = rightArray[j];
-            j++;
-        }
-        k++;
+        if (leftArray[i].prob >= rightArray[j].prob) 
+            doencas[k++] = leftArray[i++];
+        else 
+            doencas[k++] = rightArray[j++];
     }
 
-    // Copia os elementos restantes de leftArray, se houver
-    while (i < n1) {
-        doencas[k] = leftArray[i];
-        i++;
-        k++;
-    }
-
-    // Copia os elementos restantes de rightArray, se houver
-    while (j < n2) {
-        doencas[k] = rightArray[j];
-        j++;
-        k++;
-    }
+    while (i < n1) doencas[k++] = leftArray[i++];
+    while (j < n2) doencas[k++] = rightArray[j++];
 
     delete[] leftArray;
     delete[] rightArray;
 }
 
-// Função de ordenação mergeSort
 void mergeSort(Doenca *doencas, int left, int right) {
     if (left < right) {
-        int mid = left + (right - left) / 2; // Calcula o ponto médio
-
-        mergeSort(doencas, left, mid);  // Ordena a primeira metade
-        mergeSort(doencas, mid + 1, right); // Ordena a segunda metade
-
-        merge(doencas, left, mid, right);  // Mescla as duas metades ordenadas
+        int mid = left + (right - left) / 2;
+        mergeSort(doencas, left, mid);
+        mergeSort(doencas, mid + 1, right);
+        merge(doencas, left, mid, right);
     }
 }
 
@@ -154,10 +176,8 @@ int main(int argc, char* argv[]) {
     auto duration = duration_cast<microseconds>(end - start);
     cout << "Tempo para rodar o código: " << duration.count() << " microsegundos\n";
     
-    // Libera memória
-    for (int i = 0; i < num_doencas; i++) {
-        delete[] doencas[i].genes;
-    }
+    output.close();
+    for (int i = 0; i < num_doencas; i++) delete[] doencas[i].genes;
     delete[] doencas;
     
     return 0;
