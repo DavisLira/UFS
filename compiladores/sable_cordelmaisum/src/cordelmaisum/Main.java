f657c3affbfa7e040b4823874b4dd38fd7e3242d
Davi Lira <davis.lira08@gmail.com>
2025-11-17 23:31:52 -0300

    sable

---
+++ compiladores/sable_cordelmaisum/src/cordelmaisum/Main.java
@@ -0,0 +1,29 @@
+package cordelmaisum;
+import cordelmaisum.lexer.*;
+import cordelmaisum.node.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args)
+	{
+		try
+		{
+			String arquivo = "codes/calculadoras.cmu";
+
+			Lexer lexer =
+					new Lexer(
+							new PushbackReader(  
+									new FileReader(arquivo), 1024)); 
+			Token token;
+			while(!((token = lexer.next()) instanceof EOF)) {
+				System.out.println(token.getClass());
+				System.out.println(" ( "+token.toString()+")");
+			}
+		}
+		catch(Exception e)
+		{
+			System.out.println(e.getMessage());
+		}
+	}
+}
